53a565aeaeac8cd2a29073f7263dee1c51832a4b
Jon Sagara <jon@sagarasoftware.com>
2021-11-24 10:17:46 -0800

    Added PDF generating code.

--- src/SyncfusionGenerateLargePdf/Program.cs
+++ src/SyncfusionGenerateLargePdf/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SyncfusionGenerateLargePdf;
+
+
+//
+// All times are from my local dev machine.
+//
+
+// Uncomment one line at a time.
+// The method will launch the generated PDF in Chrome.
+
+GeneratePdfHelper.GenerateGridPdf(itemCount: 10);             // 00:00:00.42 
+//GeneratePdfHelper.GenerateGridPdf(itemCount: 100);            // 00:00:00.52
+//GeneratePdfHelper.GenerateGridPdf(itemCount: 1_000);          // 00:00:01.53
+//GeneratePdfHelper.GenerateGridPdf(itemCount: 10_000);         // 00:00:12.18
+//GeneratePdfHelper.GenerateGridPdf(itemCount: 100_000);        // 00:10:07.12
+//GeneratePdfHelper.GenerateGridPdf(itemCount: 1_600_000);      // Cancelled after 45 minutes and only ~250k rows.
